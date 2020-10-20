f0240e8467b32128e1fc6ca96a8c03cf672f2cb0
Miles Rosen <miles.rosen@gtb.com>
2020-10-20 11:30:40 +0100

    updated bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -7,7 +7,7 @@ function Bio(){
         <div className="Bio">
             <h3>I'm <span className="highlight">Miles</span>.</h3>
             <h4>I'm a graduate of General Assembly where I started coding in January 2019.</h4>
-            <h4>Since then I've become a full time, full stack developer.</h4>
+            <h4>Since then I've become a full time, full stack developer, joining GTB in September 2019.</h4>
             <h4>If you'd like to get in touch head to the <Link className="linkFromBio"to="/contact">contact</Link> page.</h4>
 
         </div>
