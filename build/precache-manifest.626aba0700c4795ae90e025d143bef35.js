65deee8dd83f59e39b09fff4ec9dc4593a989dd1
Miles Rosen <miles.rosen@gtb.com>
2021-02-03 13:21:53 +0000

    comitting changes to ghpages

---
+++ build/precache-manifest.626aba0700c4795ae90e025d143bef35.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd9ff65565ce57c44554825b2ad0b71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd3e2d343ca6e6f3ac56",
+    "url": "/static/css/main.21c7a386.chunk.css"
+  },
+  {
+    "revision": "378ea479bdeae4c582f9",
+    "url": "/static/js/2.a027dc0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a027dc0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd3e2d343ca6e6f3ac56",
+    "url": "/static/js/main.99581d5d.chunk.js"
+  },
+  {
+    "revision": "45eebcf774db431fa1c9",
+    "url": "/static/js/runtime-main.dd6854e9.js"
+  },
+  {
+    "revision": "ca2f283e5a34ab6bea0355ca21b9b3cc",
+    "url": "/static/media/benStokesLogo.ca2f283e.png"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/static/media/close.cad70081.svg"
+  },
+  {
+    "revision": "2d9b09b1746f814fa8c9d2becdf76f3e",
+    "url": "/static/media/indianaHamesLogo.2d9b09b1.png"
+  },
+  {
+    "revision": "4aaf92d0ed86d1b1165581081977af0d",
+    "url": "/static/media/open-menu.4aaf92d0.svg"
+  },
+  {
+    "revision": "f9d05e381305d2df66650f709f6d3ce5",
+    "url": "/static/media/pammyLogo.f9d05e38.png"
+  },
+  {
+    "revision": "d630abcba7ed445d36025738e92d3ff1",
+    "url": "/static/media/playerConnectLogo.d630abcb.png"
+  }
+]);
